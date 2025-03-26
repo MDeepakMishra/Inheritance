fe38c0ff579b67288da1325527013235e287cf24
deep misra <deepakmishra93152@gmail.com>
2025-03-26 12:08:54 +0530

    first commit

--- Multilevel.java
+++ Multilevel.java
@@ -1,11 +1,11 @@
 class Animal{  
-    void eat(){System.out.println("eating...");}  
+    void eat(){System.out.println(" i am eating...");}  
     }  
     class Dog extends Animal{  
-    void bark(){System.out.println("barking...");}  
+    void bark(){System.out.println("dog is barking...");}  
     }  
     class BabyDog extends Dog{  
-    void weep(){System.out.println("weeping...");}  
+    void weep(){System.out.println("i am weeping...");}  
     }  
     public class Multilevel{  
     public static void main(String args[]){  
